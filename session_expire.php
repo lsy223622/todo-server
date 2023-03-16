3a67193e70eae695abcf752f8f801f0b3fec2140
lsy223622 <lsy223622@outlook.com>
2023-03-16 14:51:37 +0800

    Update session_expire.php

--- session_expire.php
+++ session_expire.php
@@ -1,6 +1,7 @@
 <?php
 require_once('db_credentials.php');
 
+// 连接数据库
 $conn_todo = new mysqli("localhost", DB_USER, DB_PASS, DB_NAME);
 
 // 检测连接
@@ -8,7 +9,7 @@ if ($conn_todo->connect_error) {
     die("连接失败: " . $conn_todo->connect_error);
 }
 
-// 把所有过期的 session 的 open 置为 0
+// 把所有过期的 Session 的 open 置为 0
 $query = $conn_todo->prepare("UPDATE sessions SET Open = 0 WHERE Open = 1 AND ExpiryTime < NOW()");
 $query->execute();
 $query->close();
