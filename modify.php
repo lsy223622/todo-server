7c5f86560545793a2939ce598a491d508ff292dc
lsy223622 <lsy223622@outlook.com>
2023-03-16 14:22:55 +0800

    Create modify.php

---
+++ modify.php
@@ -0,0 +1,27 @@
+<?php
+require_once('authconnect.php');
+
+// 获取 todoInfo
+$todoInfo = $_GET['todoInfo'];
+if (!isset($todoInfo) || empty($todoInfo)) {
+    http_response_code(400);
+    die("Invalid todoInfo.");
+}
+
+$todoInfo = json_decode($todoInfo, true);
+
+// 根据 Number 修改 todo
+$query = $conn_todo->prepare("UPDATE todos SET Title = ?, Content = ?, Deadline = ?, Priority = ?, Finished = ? WHERE UserID = ? AND Number = ?");
+$title = mysqli_real_escape_string($conn_todo, $todoInfo['title']);
+$content = mysqli_real_escape_string($conn_todo, $todoInfo['content']);
+$deadline = mysqli_real_escape_string($conn_todo, $todoInfo['deadline']);
+$priority = mysqli_real_escape_string($conn_todo, $todoInfo['priority']);
+$finished = mysqli_real_escape_string($conn_todo, $todoInfo['finished']);
+$number = mysqli_real_escape_string($conn_todo, $todoInfo['number']);
+$query->bind_param("sssiiii", $title, $content, $deadline, $priority, $finished, $userId, $number);
+$query->execute();
+$query->close();
+
+$conn_todo->close();
+echo ("OK")
+?>
