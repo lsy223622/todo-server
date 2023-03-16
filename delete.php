ea96f04171006c0dd76a5512afd21bc0b66a69d1
lsy223622 <lsy223622@outlook.com>
2023-03-16 14:28:34 +0800

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+require_once('authconnect.php');
+
+// 获取 number
+$number = $_GET['number'];
+if (!isset($number) || empty($number)) {
+    http_response_code(400);
+    die("Invalid number.");
+}
+
+// 根据 Number 删除 todo
+$query = $conn_todo->prepare("DELETE FROM todos WHERE UserID = ? AND Number = ?");
+$query->bind_param("ii", $userId, $number);
+$query->execute();
+$query->close();
+
+$conn_todo->close();
+echo ("OK")
+?>
